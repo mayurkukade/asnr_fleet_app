cf572294201d87e49c2fd695b0350871b420059a
mayurkukade <mayurkukade@opticalarc.com>
2023-05-22 17:02:33 +0530

    update

--- client/src/components/Fleet/FleetManegment.jsx
+++ client/src/components/Fleet/FleetManegment.jsx
@@ -1,7 +1,9 @@
 import { useVendorDetailsQuery } from "../../api/vendorSlice";
+import { useState } from "react";
 const FleetManegment = () => {
   const { data, isLoading, isError } = useVendorDetailsQuery();
-  console.log(data);
+
+ console.log(data)
   return (
     <>
       <h1>Fleet Manegment</h1>
