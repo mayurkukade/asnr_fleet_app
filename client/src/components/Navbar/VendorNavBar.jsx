03848747b13e4dd78b89f42b72e8ff678f1af82e
Aksh-007 <akshaybarapatre54@gmail.com>
2023-05-31 11:12:15 +0530

    Fix- added navbar UI for vendors

--- client/src/components/Navbar/VendorNavBar.jsx
+++ client/src/components/Navbar/VendorNavBar.jsx
@@ -1,25 +1,34 @@
-
-import { Outlet } from "react-router-dom";
+import { Outlet, json } from "react-router-dom";
 
 import { Box, Text } from "@chakra-ui/react";
-
+// import Avtar from "./Avtar";
+import { Avatar } from "@chakra-ui/react";
 
 const VendorNavBar = () => {
+  // const data = localStorage.getItem("userInfo");
+  // console.log(JSON.parse(data).results.user[0])
+  // const {username,}
   return (
     <>
-      <Box className="nav">
-            <Box className="searchbar">
-              <Text fontSize='2xl' fontWeight="medium">ASNR Fleet App</Text >
-            </Box>
-          
+      <Box className="nav" >
+        <Box className="searchbar">
+          <Text fontSize="2xl" fontWeight="medium">
+            ASNR Fleet App
+          </Text>
+        </Box>
+        <Box className="card" px={"20px"}>
+          <Box mr={'50px'} fontSize="l" fontWeight="medium">
+            <Text>Vendor&apos;s name : </Text>
+            <Text>Vendor&apos;s Id :</Text>
           </Box>
+          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+        </Box>
+      </Box>
+      <hr />
 
-          <hr />
-    
-          <Outlet/>
-      
+      <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default VendorNavBar
+export default VendorNavBar;
